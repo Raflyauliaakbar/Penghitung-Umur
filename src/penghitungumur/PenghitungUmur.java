d112babbab7846a2f35e5ba8ee70c01b8dc2aa42
Akbar2031 <mraflyauliaakbar03@gmail.com>
2024-10-30 16:14:58 +0800

    PenghitungUmur

---
+++ src/penghitungumur/PenghitungUmur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package penghitungumur;
+
+/**
+ *
+ * @author SSD TURBO_BC
+ */
+public class PenghitungUmur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
